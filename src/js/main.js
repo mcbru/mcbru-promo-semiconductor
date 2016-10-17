0d6552de656c3fc8f2cb20700dbeccf1f52be7ac
Sam Templeman <samuel.templeman@me.com>
2016-10-16 19:22:23 -0700

    Consolidate waypoints

--- src/js/main.js
+++ src/js/main.js
@@ -10,8 +10,14 @@ $(function(){
   //   // odometer.innerHTML = 15166000;
   // }, 3000);
 
+  // var video = $('video');
+  // video.addEventListener('ended',function(){
+  //     // video.load();
+  //     console.log('video ended');
+  // },false);
+
   window.odometerOptions = {
-    duration: 6 * 1000
+    duration: 5 * 1000
   };
 
   var container5 = $('.odometer');
@@ -22,38 +28,79 @@ $(function(){
     }
   })
 
+  // var container6 = $('.c-band--image__1');
+  // var container6_elements = $('.c-band--image__1 h1, .c-band--image__1 svg');
+  // var inview6 = new Waypoint.Inview({
+  //   element: container6,
+  //   enter: function(direction) {
+  //     container6_elements.addClass('animated').addClass('fadeIn').css({'animation-delay': '1s'});
+  //   }
+  // })
 
-  var container1 = $('.c-band--image__2');
+  // var container7 = $('.o-wrapper--animate');
+  // // var container6_elements = $('.c-band--image__1 h1, .c-band--image__1 svg');
+  // var inview7 = new Waypoint.Inview({
+  //   element: container7,
+  //   enter: function(direction) {
+  //     container7.addClass('animated').addClass('fadeIn').css({'animation-delay': '1s'});
+  //   }
+  // })
+
+  function onScrollInit(items, trigger) {
+    items.each(function() {
+      var osElement = $(this);
+        // osAnimationClass = osElement.attr('data-os-animation'),
+        // osAnimationDelay = osElement.attr('data-os-animation-delay');
+        osElement.css({
+          'animation-delay': '0.2s'
+        });
+
+      var osTrigger = (trigger) ? trigger : osElement;
+      osTrigger.waypoint(function(){
+        osElement.addClass('animated').addClass('fadeIn');
+        console.log(osElement);
+      }, {
+        // triggerOnce: true,
+        // context:"document.body",
+        offset: '80%;'
+      });
+    });
+  }
+
+  onScrollInit($('.o-wrapper--animate'));
+
+
+  // var container1 = $('.c-band--image__2');
   // var container1_elements = $('.c-band--image__2 h2, .c-band--image__2 p');
-  var inview1 = new Waypoint.Inview({
-    element: container1,
-    enter: function(direction) {
-      container1.find('*').addClass('animated').addClass('fadeIn').css({'animation-delay': '1s'});
-      // console.log(container1);
-      // console.log(container1_elements);
-      // console.log('Enter triggered with direction ' + direction);
-    }//,
-    // entered: function(direction) {
-    //   console.log('Entered triggered with direction ' + direction)
-    // },
-    // exit: function(direction) {
-    //   console.log('Exit triggered with direction ' + direction)
-    // },
-    // exited: function(direction) {
-    //   console.log('Exited triggered with direction ' + direction)
-    // }
-  })
+  // var inview1 = new Waypoint.Inview({
+  //   element: container1,
+  //   enter: function(direction) {
+  //     container1_elements.addClass('animated').addClass('fadeIn').css({'animation-delay': '1s'});
+  //     // console.log(container1);
+  //     // console.log(container1_elements);
+  //     // console.log('Enter triggered with direction ' + direction);
+  //   }//,
+  //   // entered: function(direction) {
+  //   //   console.log('Entered triggered with direction ' + direction)
+  //   // },
+  //   // exit: function(direction) {
+  //   //   console.log('Exit triggered with direction ' + direction)
+  //   // },
+  //   // exited: function(direction) {
+  //   //   console.log('Exited triggered with direction ' + direction)
+  //   // }
+  // })
 
-  var container2 = $('.c-band--image__3');
-  var inview2 = new Waypoint.Inview({
-    element: container2,
-    enter: function(direction) {
-      container2.find('*').addClass('animated').addClass('fadeIn').css({'animation-delay': '1s'});
-      // console.log(container2);
-      // console.log(container1_elements);
-      // console.log('Enter triggered with direction ' + direction);
-    }
-  })
+  // var container2 = $('.c-band--image__3');
+  // var inview2 = new Waypoint.Inview({
+  //   element: container2,
+  //   enter: function(direction) {
+  //     container2.find('*').addClass('animated').addClass('fadeIn').css({'animation-delay': '1s'});
+  //     // console.log(container2);
+  //     // console.log(container1_elements);
+  //     // console.log('Enter triggered with direction ' + direction);
+  //   }
+  // })
 
   var container3 = $('.bar-group--1');
   var container3_element = $('.bar-group--1 .bar');
@@ -61,6 +108,7 @@ $(function(){
     element: container3,
     enter: function(direction) {
       container3_element.addClass('bar-animated').css({'animation-delay': '1s'});
+      $('.c-callout--1').addClass('fadeIn').addClass('animated').css({'animation-delay': '1s'});
     }
   })
 
@@ -70,49 +118,7 @@ $(function(){
     element: container4,
     enter: function(direction) {
       container4_element.addClass('bar-animated').css({'animation-delay': '1s'});
+      $('.c-callout--2').addClass('fadeIn').addClass('animated').css({'animation-delay': '1s'});
     }
   })
-
-  var container6 = $('.c-band--image__1');
-  var inview6 = new Waypoint.Inview({
-    element: container6,
-    enter: function(direction) {
-      container6.find('*').addClass('animated').addClass('fadeIn').css({'animation-delay': '1s'});
-    }
-  })
-
-// var waypoint = new Waypoint({
-//   element: document.getElementById('waypoint'),
-//   handler: function(direction) {
-//     console.log('Scrolled to waypoint!')
-//   },
-//   offset: 128
-// })
-
-
-  // function onScrollInit(items, trigger) {
-  //   items.each(function() {
-  //     var osElement = $(this),
-  //       osAnimationClass = osElement.attr('data-os-animation'),
-  //       osAnimationDelay = osElement.attr('data-os-animation-delay');
-  //       osElement.css({
-  //         '-webkit-animation-delay':  osAnimationDelay,
-  //         '-moz-animation-delay':     osAnimationDelay,
-  //         'animation-delay':          osAnimationDelay
-  //       });
-  //
-  //     var osTrigger = (trigger) ? trigger : osElement;
-  //     osTrigger.waypoint(function(){
-  //       osElement.addClass('animated').addClass(osAnimationClass);
-  //       console.log(osElement);
-  //     }, {
-  //       // triggerOnce: true,
-  //       // context:"document.body",
-  //       offset: '10%;'
-  //     });
-  //   });
-  // }
-
-  // onScrollInit($('.c-band'));
-
 });
